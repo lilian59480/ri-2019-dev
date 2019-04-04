8a30ababc9e0a2f0fe20b4c533dd7ebca6784e06
Lilian Petitpas <lilian.petitpas@outlook.com>
2019-04-04 14:33:45 +0000

    [skip ci] Create new build

---
+++ js/navbar.js
@@ -0,0 +1,74 @@
+/// @ts-check
+
+/**
+ * Load navbar menu toggles for mobile
+ * @param {HTMLElement} element
+ */
+const navbarToggles = (element) => {
+	// Get the target from the "data-target" attribute
+	const target = document.getElementById(element.dataset.target);
+	element.parentElement.classList.toggle("navbar");
+	element.parentElement.classList.toggle("is-primary");
+
+	element.classList.toggle('is-active');
+	target.classList.toggle('is-active');
+}
+
+/**
+ * Set navbar transparent if under scrollValue
+ * @param {HTMLElement} element
+ * @param {number} scrollValue
+ */
+const scrollBackground = (element, scrollValue) => {
+	const scroll = window.scrollY || window.pageYOffset;
+	if (scroll < scrollValue) {
+		element.classList.add('is-transparent-color');
+		element.classList.remove('is-primary');
+	} else {
+		element.classList.add('is-primary');
+		element.classList.remove('is-transparent-color');
+	}
+};
+
+// Enable navbar-burger buttons
+window.addEventListener('load', () => {
+	// Get all "navbar-burger" elements
+	const navbarBurgers = document.getElementsByClassName('navbar-burger');
+	const navbarButtonsBurgers = document.getElementsByClassName('navbar-end');
+
+	Array.from(navbarBurgers).forEach((/** @type {HTMLElement} */ navbarElement) => {
+		navbarElement.addEventListener('click', () => {
+			navbarToggles(navbarElement);
+		});
+
+		Array.from(navbarButtonsBurgers).forEach((/** @type {HTMLElement} */ content) => {
+			content.addEventListener('click', () => {
+				if (window.innerWidth < 1088)
+					navbarToggles(navbarElement);
+			});
+		});
+	});
+
+	console.log("[IG2I-navbar] Enabled %s navbar-burger button", navbarBurgers.length);
+
+});
+
+// Enable scrolling transparency
+window.addEventListener('load', () => {
+
+	const navbars = document.getElementsByClassName('navbar');
+
+	const scrollValue = 100;
+
+	Array.from(navbars).forEach((/** @type {HTMLElement} */ element) => {
+		// Do a first call, to set the right value now
+		scrollBackground(element, scrollValue);
+		window.addEventListener('scroll', () => {
+			scrollBackground(element, scrollValue);
+		});
+	});
+
+	console.log("[IG2I-navbar] Enabled %s scrolling transparency", navbars.length);
+
+
+});
---
+++ js/video.js
@@ -0,0 +1,25 @@
+/// @ts-check
+
+/**
+ * Play the video when it appears on the screen
+ */
+const playVideoOnScroll = () => {
+    const bounds = document.querySelector("video").getBoundingClientRect()
+    const isViewable = bounds.top < window.innerHeight * 0.5
+        && bounds.bottom >= window.innerHeight * 0.5
+
+    if (isViewable)
+        document.querySelector("video").play();
+
+    if (!isViewable)
+        document.querySelector("video").pause();
+};
+
+// Add Event onScroll
+window.addEventListener('scroll', () => {
+    playVideoOnScroll();
+});
+
+window.addEventListener('load', () => {
+    document.querySelector("video").play();
+})
